a8015ca007b3c95aef54be804a40aabe738a2614
Michael Barela <michael.d.barela@gmail.com>
2021-12-25 14:59:33 -0800

    re-do

--- Jetson_Nano/scripts/demo-record.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ffmpeg -video_size 1919x1079 -framerate 30 -f x11grab -i :0.0+1,1 /home/jetson-nano/Desktop/demo2-jetson.mkv
-
-ffmpeg -video_size 1919x1079 -framerate 30 -f x11grab -i :0.0+1,1 /home/jetson-nano/Desktop/demo2-jetson.mkv
